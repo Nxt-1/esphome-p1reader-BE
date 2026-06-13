29aa4884dcb448e9add0e249c88874f832c9639e
Nxt-1 <aerts.arne@gmail.com>
2026-06-13 18:17:23 +0200

    Added compatibility for Fluvius meters with multiple tariffs codes as 1.8.x

--- components/p1reader/parsed_message.h
+++ components/p1reader/parsed_message.h
@@ -1,9 +1,7 @@
 #pragma once
 
-namespace esphome
-{
-    namespace p1_reader
-    {
+namespace esphome {
+    namespace p1_reader {
         class ParsedMessage {
         public:
             double cumulativeActiveImport;
@@ -49,221 +47,260 @@ namespace esphome
             bool crcOk;
             uint8_t sensorsToSend;
 
-            void parseRow(const char* obisCode, const char* value)
-            {
-                double obisValue = simpleatof(value);
+            void parseRow(const char *obisCode, const char *value) {
+              double obisValue = simpleatof(value);
 
-                parseRow(obisCode, obisValue);
+              parseRow(obisCode, obisValue);
             }
 
-            void parseRow(const char* obisCode, double obisValue)
-            {
-                int obisCodeLen = strnlen(obisCode, 7);
-                
-                if (obisCode[obisCodeLen-1] == '0' &&
-                    obisCode[obisCodeLen-2] == '.' &&
-                    obisCode[obisCodeLen-4] == '.')
-                {
-                    switch (obisCodeLen)
-                    {
-                        case 5: switch(obisCode[obisCodeLen-3])
-                        {
-                            case '7': switch (obisCode[0])
-                            {
-                                case '1': 
-                                    momentaryActiveImport = obisValue;
-                                    break;
-                                case '2': 
-                                    momentaryActiveExport = obisValue;
-                                    break;
-                                case '3': 
-                                    momentaryReactiveImport = obisValue;
-                                    break;
-                                case '4': 
-                                    momentaryReactiveExport = obisValue;
-                                    break;
-                                default: break;
-                            }
+            void parseRow(const char *obisCode, double obisValue) {
+              // Belgian/Fluvius cumulative active import registers.
+              if (strcmp(obisCode, "1.8.1") == 0) {
+                cumulativeActiveImportTariff1 = obisValue;
+                hasCumulativeActiveImportTariffs = true;
+                return;
+              }
+
+              if (strcmp(obisCode, "1.8.2") == 0) {
+                cumulativeActiveImportTariff2 = obisValue;
+                hasCumulativeActiveImportTariffs = true;
+                return;
+              }
+
+              // Belgian/Fluvius cumulative active export registers.
+              if (strcmp(obisCode, "2.8.1") == 0) {
+                cumulativeActiveExportTariff1 = obisValue;
+                hasCumulativeActiveExportTariffs = true;
+                return;
+              }
+
+              if (strcmp(obisCode, "2.8.2") == 0) {
+                cumulativeActiveExportTariff2 = obisValue;
+                hasCumulativeActiveExportTariffs = true;
+                return;
+              }
+
+              int obisCodeLen = strnlen(obisCode, 7);
+
+              if (obisCode[obisCodeLen - 1] == '0' &&
+                      obisCode[obisCodeLen - 2] == '.' &&
+                      obisCode[obisCodeLen - 4] == '.') {
+                switch (obisCodeLen) {
+                  case 5:
+                    switch (obisCode[obisCodeLen - 3]) {
+                      case '7':
+                        switch (obisCode[0]) {
+                          case '1':
+                            momentaryActiveImport = obisValue;
                             break;
-                            case '8': switch (obisCode[0])
-                            {
-                                case '1': 
-                                    cumulativeActiveImport = obisValue;
-                                    break;
-                                case '2': 
-                                    cumulativeActiveExport = obisValue;
-                                    break;
-                                case '3': 
-                                    cumulativeReactiveImport = obisValue;
-                                    break;
-                                case '4': 
-                                    cumulativeReactiveExport = obisValue;
-                                    break;
-                                default: break;
-                            }
+                          case '2':
+                            momentaryActiveExport = obisValue;
                             break;
-                            default: break;
+                          case '3':
+                            momentaryReactiveImport = obisValue;
+                            break;
+                          case '4':
+                            momentaryReactiveExport = obisValue;
+                            break;
+                          default:
+                            break;
                         }
                         break;
-                        case 6: if (obisCode[obisCodeLen-3] == '7') {
-                            switch(obisCode[1]) {
-                                case '1': switch (obisCode[0])
-                                {
-                                    case '2': 
-                                        momentaryActiveImportL1 = obisValue;
-                                        break;
-                                    case '3': 
-                                        currentL1 = obisValue;
-                                        break;
-                                    case '4': 
-                                        momentaryActiveImportL2 = obisValue;
-                                        break;
-                                    case '5': 
-                                        currentL2 = obisValue;
-                                        break;
-                                    case '6': 
-                                        momentaryActiveImportL3 = obisValue;
-                                        break;
-                                    case '7': 
-                                        currentL3 = obisValue;
-                                        break;
-                                    default: break;
-                                }
-                                break;
-                                case '2': switch (obisCode[0])
-                                {
-                                    case '2': 
-                                        momentaryActiveExportL1 = obisValue;
-                                        break;
-                                    case '3': 
-                                        voltageL1 = obisValue;
-                                        break;
-                                    case '4': 
-                                        momentaryActiveExportL2 = obisValue;
-                                        break;
-                                    case '5': 
-                                        voltageL2 = obisValue;
-                                        break;
-                                    case '6': 
-                                        momentaryActiveExportL3 = obisValue;
-                                        break;
-                                    case '7': 
-                                        voltageL3 = obisValue;
-                                        break;
-                                    default: break;
-                                }
-                                break;
-                                case '3': switch (obisCode[0])
-                                {
-                                    case '2': 
-                                        momentaryReactiveImportL1 = obisValue;
-                                        break;
-                                    case '4': 
-                                        momentaryReactiveImportL2 = obisValue;
-                                        break;
-                                    case '6': 
-                                        momentaryReactiveImportL3 = obisValue;
-                                        break;
-                                    default: break;
-                                }
-                                break;
-                                case '4': switch (obisCode[0])
-                                {
-                                    case '2': 
-                                        momentaryReactiveExportL1 = obisValue;
-                                        break;
-                                    case '4': 
-                                        momentaryReactiveExportL2 = obisValue;
-                                        break;
-                                    case '6': 
-                                        momentaryReactiveExportL3 = obisValue;
-                                        break;
-                                    default: break;
-                                }
-                                break;
-                                default: break;
-                            }
+                      case '8':
+                        switch (obisCode[0]) {
+                          case '1':
+                            cumulativeActiveImport = obisValue;
+                            break;
+                          case '2':
+                            cumulativeActiveExport = obisValue;
+                            break;
+                          case '3':
+                            cumulativeReactiveImport = obisValue;
+                            break;
+                          case '4':
+                            cumulativeReactiveExport = obisValue;
+                            break;
+                          default:
+                            break;
                         }
                         break;
-                        default: break;
+                      default:
+                        break;
                     }
+                    break;
+                  case 6:
+                    if (obisCode[obisCodeLen - 3] == '7') {
+                      switch (obisCode[1]) {
+                        case '1':
+                          switch (obisCode[0]) {
+                            case '2':
+                              momentaryActiveImportL1 = obisValue;
+                              break;
+                            case '3':
+                              currentL1 = obisValue;
+                              break;
+                            case '4':
+                              momentaryActiveImportL2 = obisValue;
+                              break;
+                            case '5':
+                              currentL2 = obisValue;
+                              break;
+                            case '6':
+                              momentaryActiveImportL3 = obisValue;
+                              break;
+                            case '7':
+                              currentL3 = obisValue;
+                              break;
+                            default:
+                              break;
+                          }
+                          break;
+                        case '2':
+                          switch (obisCode[0]) {
+                            case '2':
+                              momentaryActiveExportL1 = obisValue;
+                              break;
+                            case '3':
+                              voltageL1 = obisValue;
+                              break;
+                            case '4':
+                              momentaryActiveExportL2 = obisValue;
+                              break;
+                            case '5':
+                              voltageL2 = obisValue;
+                              break;
+                            case '6':
+                              momentaryActiveExportL3 = obisValue;
+                              break;
+                            case '7':
+                              voltageL3 = obisValue;
+                              break;
+                            default:
+                              break;
+                          }
+                          break;
+                        case '3':
+                          switch (obisCode[0]) {
+                            case '2':
+                              momentaryReactiveImportL1 = obisValue;
+                              break;
+                            case '4':
+                              momentaryReactiveImportL2 = obisValue;
+                              break;
+                            case '6':
+                              momentaryReactiveImportL3 = obisValue;
+                              break;
+                            default:
+                              break;
+                          }
+                          break;
+                        case '4':
+                          switch (obisCode[0]) {
+                            case '2':
+                              momentaryReactiveExportL1 = obisValue;
+                              break;
+                            case '4':
+                              momentaryReactiveExportL2 = obisValue;
+                              break;
+                            case '6':
+                              momentaryReactiveExportL3 = obisValue;
+                              break;
+                            default:
+                              break;
+                          }
+                          break;
+                        default:
+                          break;
+                      }
+                    }
+                    break;
+                  default:
+                    break;
                 }
+              }
             }
 
             // Limitations: 
             //   Numbers larger than 2G will fail (but spec only goes to 99999999.999 so ok)
             //   And numbers have no more than 3 decimals in the spec.
             //   spec == Swedish spec for H1
-            double simpleatof(const char* value)
-            {
-                double decFactors[10] = {10.0, 100.0, 1000.0, 100000.0,
-                                        1000000.0, 10000000.0, 100000000.0,
-                                        1000000000.0, 10000000000.0,
-                                        100000000000.0};
-                int idx = 0;
-                int intPart = 0;
-                bool negative = false;
+            double simpleatof(const char *value) {
+              double decFactors[10] = {10.0, 100.0, 1000.0, 100000.0,
+                                       1000000.0, 10000000.0, 100000000.0,
+                                       1000000000.0, 10000000000.0,
+                                       100000000000.0};
+              int idx = 0;
+              int intPart = 0;
+              bool negative = false;
 
-                if (value[idx] == '-')
-                {
-                    negative = true;
-                    idx++;
-                }
+              if (value[idx] == '-') {
+                negative = true;
+                idx++;
+              }
 
-                while (value[idx] != '.')
-                {
-                    intPart = intPart*10 + (value[idx]-'0');
-                    idx++;
-                }
+              while (value[idx] != '.') {
+                intPart = intPart * 10 + (value[idx] - '0');
+                idx++;
+              }
 
-                int len = strlen(value), startIdx = ++idx;
-                int decPart = 0;
-                while (idx < len)
-                {
-                    decPart = decPart*10 + (value[idx]-'0');
-                    idx++;
-                }
+              int len = strlen(value), startIdx = ++idx;
+              int decPart = 0;
+              while (idx < len) {
+                decPart = decPart * 10 + (value[idx] - '0');
+                idx++;
+              }
 
-                if (negative)
-                {
-                    return -(intPart + decPart/decFactors[len-startIdx-1]);
-                }
-                else
-                {
-                    return intPart + decPart/decFactors[len-startIdx-1];
-                }
+              if (negative) {
+                return -(intPart + decPart / decFactors[len - startIdx - 1]);
+              } else {
+                return intPart + decPart / decFactors[len - startIdx - 1];
+              }
             }
 
-            void initNewTelegram()
-            {
-                crc = 0x0000;
-                telegramComplete = false;
-                crcOk = false;
-                sensorsToSend = 26;
+            void initNewTelegram() {
+              cumulativeActiveImportTariff1 = 0.0;
+              cumulativeActiveImportTariff2 = 0.0;
+              cumulativeActiveExportTariff1 = 0.0;
+              cumulativeActiveExportTariff2 = 0.0;
+              hasCumulativeActiveImportTariffs = false;
+              hasCumulativeActiveExportTariffs = false;
+
+              crc = 0x0000;
+              telegramComplete = false;
+              crcOk = false;
+              sensorsToSend = 26;
             }
 
-            void updateCrc16(uint8_t a)
-            {
-                int i;
-                crc ^= a;
-                for (i = 0; i < 8; ++i)
-                {
-                    if (crc & 1)
-                    {
-                        crc = (crc >> 1) ^ 0xA001;
-                    }
-                    else
-                    {
-                        crc = (crc >> 1);
-                    }
+            void updateCrc16(uint8_t a) {
+              int i;
+              crc ^= a;
+              for (i = 0; i < 8; ++i) {
+                if (crc & 1) {
+                  crc = (crc >> 1) ^ 0xA001;
+                } else {
+                  crc = (crc >> 1);
                 }
+              }
             }
-            
-            void checkCrc(uint16_t crcFromMessage)
-            {
-                crcOk = crc == crcFromMessage;
-                telegramComplete = true;
+
+            void checkCrc(uint16_t crcFromMessage) {
+              // Prefer Belgian tariff registers when they were present.
+              // Otherwise, retain values parsed from 1.8.0 and 2.8.0.
+              if (hasCumulativeActiveImportTariffs) {
+                cumulativeActiveImport =
+                        cumulativeActiveImportTariff1 +
+                                cumulativeActiveImportTariff2;
+              }
+
+              if (hasCumulativeActiveExportTariffs) {
+                cumulativeActiveExport =
+                        cumulativeActiveExportTariff1 +
+                                cumulativeActiveExportTariff2;
+              }
             }
-        };
-    }
+
+            telegramComplete = true;
+        }
+    };
+}
 }
